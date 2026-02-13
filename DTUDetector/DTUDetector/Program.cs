9b2adad17c95c9848cba368d6d821a0b86cd4a18
Muhammad Mahmud <muhammad.bin.mahmud@gmail.com>
2026-02-14 00:16:19 +0800

    First file upload

---
+++ DTUDetector/DTUDetector/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace DTUDetector
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var root = @"C:Projects"; //Update this path
+
+            var files = Directory
+                .GetFiles(root, "appsettings*.json", SearchOption.AllDirectories)
+                .Where(f => !f.Contains(@"\bin\") && !f.Contains(@"\obj\"));
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    var json = File.ReadAllText(file);
+
+                    var options = new JsonDocumentOptions
+                    {
+                        AllowTrailingCommas = true
+                    };
+
+                    using (var doc = JsonDocument.Parse(json, options))
+                    {
+                        if (HasSkuInfo(doc.RootElement))
+                        {
+                            var projectName = new DirectoryInfo(Path.GetDirectoryName(file)).Name;
+                            Console.WriteLine($"{projectName} | {file}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {file}");
+                }
+            }
+        }
+
+        public static bool HasSkuInfo(JsonElement element)
+        {
+            foreach (var prop in element.EnumerateObject())
+            {
+                if (prop.Value.ValueKind == JsonValueKind.Object)
+                {
+                    if (HasSkuInfo(prop.Value))
+                        return true;
+                }
+                else
+                {
+                    var name = prop.Name.ToLower();
+
+                    if (name.Contains("sku"))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
